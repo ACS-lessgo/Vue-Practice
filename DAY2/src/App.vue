1e44ba99a90f4ecf03089f0001846e9bbcb93951
Ankush  C S <104709240+ACS-lessgo@users.noreply.github.com>
2023-09-14 13:45:18 +0530

    Add files via upload

---
+++ DAY2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-container">
+    <!-- <blog Title="Title 1" Description="Content 1"></blog>
+    <blog Title="Title 2" Description="Content 2"></blog>
+    <blog Title="Title 3" Description="Content 3"></blog> -->
+    <directive></directive>
+  </div>
+</template>
+
+<script setup>
+// import blog from "./components/blog.vue";
+import directive from "./components/directive.vue"
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap");
+
+body {
+  margin: 0px;
+  height: 100vh;
+  font-family: "Roboto", sans-serif;
+}
+
+#app {
+  height: 100%;
+}
+
+.main-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ DAY2/src/components/directive.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Directive</h1>
+  <!-- v-bind -->
+  <a :href="url">Figma</a>
+  <!-- v-html -->
+  <span v-html="rawHtml"></span>
+  <!-- v-for -->
+  <ul>
+    <li v-for="(item,index) in items" :key="index">{{ item.id +" "+item.name }}</li>
+  </ul>
+  <!-- v-model -->
+  <h1>{{ message }}</h1>
+  <input type="text" v-model="message" placeholder="enter text">
+</template>
+
+<script setup>
+import { ref } from "vue";
+const url = ref(
+  "https://www.figma.com/file/ne9I7ZWuxSMbdC4ni3EclH/DsTube?type=design&node-id=0-1&mode=design&t=kTL8FP3RU900rng8-0"
+);
+const rawHtml = ref('<span style="color:red">This should be ignored</span>');
+const items = ref([
+    {id:1,name:'Abc'},
+    {id:2,name:'Def'},
+    {id:3,name:'Ghi'}
+]);
+const message = ref('');
+</script>
+
+<style></style>
